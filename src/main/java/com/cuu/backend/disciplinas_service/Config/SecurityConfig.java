e46ff59b7b2817f5b1709f47c669259be703b67e
404947_Gallo_Mateo <404947@tecnicatura.frc.utn.edu.ar>
2025-04-20 16:23:08 -0300

    SCRUM-6 se crean clases config para Seguridad y CORS, se modifica .properties y pom.xml

---
+++ src/main/java/com/cuu/backend/disciplinas_service/Config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.cuu.backend.disciplinas_service.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .cors(Customizer.withDefaults()) // usa la clase CorsGlobalFilter
+                .build();
+    }
+}
+
+
