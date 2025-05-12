34d02ae6985883e431c5d6b0f3e495f606f27e82
404947_Gallo_Mateo <404947@tecnicatura.frc.utn.edu.ar>
2025-05-11 21:08:43 -0300

    se recibe correctamente toda la info del User en sesion q devuelve Keycloak

--- src/main/java/com/cuu/backend/disciplinas_service/Services/UserServiceImpl.java
+++ src/main/java/com/cuu/backend/disciplinas_service/Services/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.cuu.backend.disciplinas_service.Models.DTOs.UserDTO;
 import com.cuu.backend.disciplinas_service.Models.Entities.Category;
 import com.cuu.backend.disciplinas_service.Models.Entities.StudentInscription;
 import com.cuu.backend.disciplinas_service.Models.Entities.User;
+import com.cuu.backend.disciplinas_service.Models.Enums.Genre;
 import com.cuu.backend.disciplinas_service.Models.Enums.Role;
 import com.cuu.backend.disciplinas_service.Repositories.CategoryRepo;
 import com.cuu.backend.disciplinas_service.Repositories.StudentInscriptionRepo;
@@ -18,6 +19,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -39,45 +42,54 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public ExpandedUserDTO syncUserFromKeycloak(Jwt jwt) {
-        //datos del User q devuelve keycloak al hacer login
+        //datos (por defecto de keycloak) del User q se devuelve luego del login
         String keycloakId = jwt.getClaimAsString("sub");
         String email = jwt.getClaimAsString("email");
         String username = jwt.getClaimAsString("preferred_username");
         String firstName = jwt.getClaimAsString("given_name");
         String lastName = jwt.getClaimAsString("family_name");
-        //LocalDate birthDate
-        //Genre genre
         Role role = this.getRoleFromJwt(jwt);
+
+        // datos personalizados (q agregue yo al User del Realm Club_Union_Unquillo) q devuelve keycloak luego del login
+        String birthDateStr = jwt.getClaimAsString("birth_date");
+        String genreStr = jwt.getClaimAsString("genre");
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate birthDate = LocalDate.parse(birthDateStr, formatter);
+        Genre genre = genreStr.startsWith("M")? Genre.MALE : Genre.FEMALE ;
 
         User currentUser = userRepo.findByKeycloakId(keycloakId)
                 //update si ya existe
                 .map(existingUser -> {
-                    existingUser.setEmail(email);
                     existingUser.setKeycloakId(keycloakId);
+                    existingUser.setRole(role);
                     existingUser.setUsername(username);
+                    existingUser.setEmail(email);
                     existingUser.setFirstName(firstName != null ? firstName : username);
                     existingUser.setLastName(lastName != null ? lastName : "-");
-                    //birthDate
-                    //genre
+                    existingUser.setBirthDate(birthDate);
+                    existingUser.setGenre(genre);
 
-                    //actualiza y devuelve el mismo User pero con los datos de mi DB
+                    //actualiza y devuelve el mismo User pero con los datos de la DB de este MS
                     return userRepo.save(existingUser);
                 })
                 // lo crea si NO existe
                 .orElseGet(() -> {
                     User newUser = new User();
-                    newUser.setEmail(email);
                     newUser.setKeycloakId(keycloakId);
+                    newUser.setRole(role);
                     newUser.setUsername(username);
+                    newUser.setEmail(email);
                     newUser.setFirstName(firstName != null ? firstName : username);
                     newUser.setLastName(lastName != null ? lastName : "-");
-                    //birthDate
-                    //genre
+                    newUser.setBirthDate(birthDate);
+                    newUser.setGenre(genre);
 
-                    //crea y devuelve el mismo User pero con los datos de mi DB
+                    //crea y devuelve el mismo User pero con los datos de la DB de este MS
                     return userRepo.save(newUser);
                 });
 
+        //agrega las categories donde esta inscripto el User
         ExpandedUserDTO expandedCurrentUser = mapper.map(currentUser, ExpandedUserDTO.class);
         expandedCurrentUser.setStudentCategories(this.getAllStudentCategories(keycloakId));
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,13 @@
 -- DISCIPLINES
 MERGE INTO disciplines (id, name, description)
 VALUES
-  ('11111111-1111-1111-1111-111111111111', 'Basquet', 'Disciplina de Basquet para todas las edades.'),
-  ('22222222-2222-2222-2222-222222222222', 'Boxeo', 'Disciplina de Boxeo para niños y adultos.');
+  ('11111111-1111-1111-1111-111111111111', 'Básquet', 'Básquet para todas las edades, con una división de Primera Categoría para los grandes talentos'),
+  ('22222222-2222-2222-2222-222222222222', 'Boxeo', 'Boxeo para recreativo para niños y Boxeo competitivo para los adultos.'),
+  ('33333333-3333-3333-3333-333333333333', 'Gimnasia Rítmica', 'Gimnasia Rítmica para todas las edades.'),
+  ('44444444-4444-4444-4444-444444444444', 'Karate Do', 'Karate del mejor nivel continental, para todas las edades.');
 
--- CATEGORIES (Basquet)
+
+-- CATEGORIES Basquet (11111111-1111-1111-1111-111111111111)
 MERGE INTO categories (id, name, description, monthly_fee, discipline_id, available_spaces, min_age, max_age, allowed_genre)
 VALUES
   ('aaaaaaa1-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'U7 - U9', 'Categoría para niños de 7 a 9 años.', 14000.00, '11111111-1111-1111-1111-111111111111', 20, 7, 9, 'MIXED'),
@@ -14,11 +17,24 @@ VALUES
   ('aaaaaaa5-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'U21', 'Categoría para adolescentes y jóvenes de 18 a 21 años.', 16000.00, '11111111-1111-1111-1111-111111111111', 25, 18, 21, 'MIXED'),
   ('aaaaaaa6-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'Primera', 'Categoría para adultos de 21 años en adelante.', 16500.00, '11111111-1111-1111-1111-111111111111', 20, 21, 40, 'MIXED');
 
--- CATEGORIES (Natación)
+-- CATEGORIES Boxeo (22222222-2222-2222-2222-222222222222)
 MERGE INTO categories (id, name, description, monthly_fee, discipline_id, available_spaces, min_age, max_age, allowed_genre)
 VALUES
   ('bbbbbbb1-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'Recreativo', 'Recreativo básica para niños.', 12000.00, '22222222-2222-2222-2222-222222222222', 0, 10, 18, 'MIXED'),
   ('bbbbbbb2-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'Competitivo', 'Competitivo avanzado para adultos.', 15000.00, '22222222-2222-2222-2222-222222222222', 0, 18, 55, 'MIXED');
+
+-- CATEGORIES Gimnasia Ritmica (33333333-3333-3333-3333-333333333333)
+MERGE INTO categories (id, name, description, monthly_fee, discipline_id, available_spaces, min_age, max_age, allowed_genre)
+VALUES
+  ('ccccccc1-cccc-cccc-cccc-cccccccccccc', 'Inicial', 'Recreativo básica para niños menores de 5 años.', 12000.00, '33333333-3333-3333-3333-333333333333', 0, 3, 5, 'MIXED'),
+  ('ccccccc2-cccc-cccc-cccc-cccccccccccc', 'Medio', 'Intermedio para niños de 6 o más años.', 12000.00, '33333333-3333-3333-3333-333333333333', 0, 6, 18, 'MIXED'),
+  ('ccccccc3-cccc-cccc-cccc-cccccccccccc', 'Avanzado', 'Nivel avanzado para mayores de 12 años.', 12000.00, '33333333-3333-3333-3333-333333333333', 0, 18, 55, 'MIXED');
+
+-- CATEGORIES Karate Do (44444444-4444-4444-4444-444444444444)
+MERGE INTO categories (id, name, description, monthly_fee, discipline_id, available_spaces, min_age, max_age, allowed_genre)
+VALUES
+  ('fffffff1-ffff-ffff-ffff-ffffffffffff', 'Karate Tradicional', 'Karate Tradicional para todas las edades, a partir de los 5 años.', 15000.00, '44444444-4444-4444-4444-444444444444', 0, 5, 55, 'MIXED'),
+  ('fffffff2-ffff-ffff-ffff-ffffffffffff', 'Deportivo', 'Karate Deportivo, a partir de los 5 años.', 15000.00, '44444444-4444-4444-4444-444444444444', 0, 5, 55, 'MIXED');
 
 
 -- SCHEDULES
@@ -29,54 +45,76 @@ VALUES
 -- basquet U7 U9
   ('aaaaaaa1-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'TUESDAY', '18:15:00', '19:15:00'),
   ('aaaaaaa1-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'THURSDAY', '18:15:00', '19:15:00'),
-
 -- basquet U11 U13
   ('aaaaaaa2-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'TUESDAY', '19:15:00', '20:15:00'),
   ('aaaaaaa2-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'THURSDAY', '19:15:00', '20:15:00'),
-
 -- basquet U15
   ('aaaaaaa3-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'TUESDAY', '19:15:00', '21:15:00'),
   ('aaaaaaa3-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'THURSDAY', '19:15:00', '21:15:00'),
   ('aaaaaaa3-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'FRIDAY', '19:30:00', '21:00:00'),
-
 -- basquet U17
   ('aaaaaaa4-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'TUESDAY', '20:15:00', '22:15:00'),
   ('aaaaaaa4-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'THURSDAY', '20:15:00', '22:15:00'),
   ('aaaaaaa4-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'FRIDAY', '19:30:00', '21:00:00'),
-
 -- basquet U21
   ('aaaaaaa5-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'TUESDAY', '22:15:00', '23:30:00'),
   ('aaaaaaa5-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'THURSDAY', '22:15:00', '23:30:00'),
-
 -- basquet PRIMERA
   ('aaaaaaa6-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'TUESDAY', '22:15:00', '23:30:00'),
   ('aaaaaaa6-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'THURSDAY', '22:15:00', '23:30:00'),
-
 
 -- boxeo Recreativo
   ('bbbbbbb1-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'MONDAY', '19:00:00', '20:00:00'),
   ('bbbbbbb1-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'WEDNESDAY', '19:00:00', '20:00:00'),
   ('bbbbbbb1-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'FRIDAY', '19:00:00', '20:00:00'),
-
 -- boxeo Competitivo
   ('bbbbbbb2-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'MONDAY', '19:00:00', '20:00:00'),
   ('bbbbbbb2-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'TUESDAY', '19:00:00', '20:00:00'),
   ('bbbbbbb2-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'WEDNESDAY', '19:00:00', '20:00:00'),
   ('bbbbbbb2-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'THURSDAY', '19:00:00', '20:00:00'),
-  ('bbbbbbb2-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'FRIDAY', '19:00:00', '20:00:00');
+  ('bbbbbbb2-bbbb-bbbb-bbbb-bbbbbbbbbbbb', 'FRIDAY', '19:00:00', '20:00:00'),
 
+-- Gimnasia Ritmica Inicial
+  ('ccccccc1-cccc-cccc-cccc-cccccccccccc', 'TUESDAY', '17:30:00', '18:30:00'),
+  ('ccccccc1-cccc-cccc-cccc-cccccccccccc', 'THURSDAY', '17:30:00', '18:30:00'),
+-- Gimnasia Ritmica Medio
+  ('ccccccc2-cccc-cccc-cccc-cccccccccccc', 'TUESDAY', '18:30:00', '19:30:00'),
+  ('ccccccc2-cccc-cccc-cccc-cccccccccccc', 'THURSDAY', '18:30:00', '19:30:00'),
+-- Gimnasia Ritmica Avanzado
+  ('ccccccc3-cccc-cccc-cccc-cccccccccccc', 'TUESDAY', '18:30:00', '20:00:00'),
+  ('ccccccc3-cccc-cccc-cccc-cccccccccccc', 'THURSDAY', '18:30:00', '20:00:00'),
+
+--Karate Do Karate Tradicional
+  ('fffffff1-ffff-ffff-ffff-ffffffffffff', 'TUESDAY', '10:00:00', '11:00:00'),
+  ('fffffff1-ffff-ffff-ffff-ffffffffffff', 'TUESDAY', '18:15:00', '19:15:00'),
+  ('fffffff1-ffff-ffff-ffff-ffffffffffff', 'TUESDAY', '19:30:00', '20:30:00'),
+  ('fffffff1-ffff-ffff-ffff-ffffffffffff', 'TUESDAY', '20:30:00', '22:00:00'),
+  ('fffffff1-ffff-ffff-ffff-ffffffffffff', 'TUESDAY', '10:00:00', '11:00:00'),
+  ('fffffff1-ffff-ffff-ffff-ffffffffffff', 'TUESDAY', '18:15:00', '19:15:00'),
+  ('fffffff1-ffff-ffff-ffff-ffffffffffff', 'TUESDAY', '19:30:00', '20:30:00'),
+  ('fffffff1-ffff-ffff-ffff-ffffffffffff', 'TUESDAY', '20:30:00', '22:00:00'),
+  ('fffffff1-ffff-ffff-ffff-ffffffffffff', 'WEDNESDAY', '18:15:00', '19:15:00'),
+  ('fffffff1-ffff-ffff-ffff-ffffffffffff', 'WEDNESDAY', '19:30:00', '20:30:00'),
+  ('fffffff1-ffff-ffff-ffff-ffffffffffff', 'WEDNESDAY', '20:30:00', '22:00:00'),
+  ('fffffff1-ffff-ffff-ffff-ffffffffffff', 'FRIDAY', '18:15:00', '19:15:00'),
+  ('fffffff1-ffff-ffff-ffff-ffffffffffff', 'FRIDAY', '19:30:00', '20:30:00'),
+  ('fffffff1-ffff-ffff-ffff-ffffffffffff', 'FRIDAY', '20:30:00', '22:00:00'),
+  -- Karate Do Deportivo
+  ('fffffff2-ffff-ffff-ffff-ffffffffffff', 'SATURDAY', '10:00:00', '12:00:00');
 
 -- USERS
 INSERT INTO users (id, keycloak_id, role, username, email, first_name, last_name, birth_date, genre)
 VALUES
+  ('11111111-1111-1111-1111-111111111111', 'a1d5ca02-e4e8-41d2-9fff-de744a3b782e', 'STUDENT', 'soyAdmin', 'aaaa@example.com', 'no', 'tampoco', '2004-10-16', 'MALE'),
   ('33333333-3333-3333-3333-333333333333', 'kc-student-uuid-11111111', 'STUDENT', 'juanjo', 'Juan@example.com', 'Juan', 'Pérez', '2010-04-15', 'MALE'),
   ('44444444-4444-4444-4444-444444444444', 'kc-teacher-uuid-22222222', 'TEACHER', 'Profe Lucia', 'Lucia@example.com', 'Lucía', 'Gómez', '1985-09-30', 'FEMALE');
 
 -- STUDENT INSCRIPTIONS
 INSERT INTO student_inscriptions (id, student_id, discipline_id, category_id)
 VALUES
-  ('55555555-5555-5555-5555-555555555555', '33333333-3333-3333-3333-333333333333', '11111111-1111-1111-1111-111111111111', 'aaaaaaa6-aaaa-aaaa-aaaa-aaaaaaaaaaaa'), -- Basquet Primera
-  ('66666666-6666-6666-6666-666666666666', '44444444-4444-4444-4444-444444444444', '22222222-2222-2222-2222-222222222222', 'bbbbbbb1-bbbb-bbbb-bbbb-bbbbbbbbbbbb'); -- Boxeo Recreativo
+  ('11111111-5555-5555-5555-555555555555', '33333333-3333-3333-3333-333333333333', '11111111-1111-1111-1111-111111111111', 'aaaaaaa6-aaaa-aaaa-aaaa-aaaaaaaaaaaa'), -- Basquet Primera
+  ('11111111-6666-6666-6666-666666666666', '44444444-4444-4444-4444-444444444444', '22222222-2222-2222-2222-222222222222', 'bbbbbbb1-bbbb-bbbb-bbbb-bbbbbbbbbbbb'), -- Boxeo Recreativo
+  ('11111111-7777-7777-7777-777777777777', '11111111-1111-1111-1111-111111111111', '44444444-4444-4444-4444-444444444444', 'fffffff1-ffff-ffff-ffff-ffffffffffff'); -- Karate Do Karate Tradicional
 
 -- TEACHER DISCIPLINES
 INSERT INTO teacher_disciplines (teacher_id, discipline_id)
